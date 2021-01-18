2d685380ae8b2e854ec2bf0a05db19a296fa507e
tolfino <39185647+tolfino@users.noreply.github.com>
2021-01-17 20:30:52 -0800

    tfw no intents

--- cringebot/bot.py
+++ cringebot/bot.py
@@ -7,14 +7,26 @@ bot = discord.Client()
 slash = SlashCommand(bot)
 
 
+def prettyprint_author(author):
+    return f'{author.name}#{author.discriminator} ({author.id})'
+
+
 @slash.slash(name='changenick')
 async def changenick(ctx: SlashContext, user: str, nick: str):
-    await bot.http.edit_member(ctx.guild.id, user, reason=f'{ctx.author.name}#{ctx.author.discriminator} ({ctx.author.id})', nick=nick)
+    author = ctx.author
+    if isinstance(author, int):
+        author = await ctx.bot.fetch_user(author)
+    await ctx.bot.http.edit_member(ctx.guild.id, user, reason=prettyprint_author(author), nick=nick)
+    await ctx.ack()
 
 
 @slash.slash(name='clearnick')
 async def clearnick(ctx: SlashContext, user: str):
-    await bot.http.edit_member(ctx.guild.id, user, reason=f'{ctx.author.name}#{ctx.author.discriminator} ({ctx.author.id})', nick=None)
+    author = ctx.author
+    if isinstance(author, int):
+        author = await ctx.bot.fetch_user(author)
+    await ctx.bot.http.edit_member(ctx.guild.id, user, reason=prettyprint_author(author), nick=None)
+    await ctx.ack()
 
 
 def main(token=os.getenv('CRINGEBOT_TOKEN')):
