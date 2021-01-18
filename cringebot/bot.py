3241f93a3269d8eea9c8830e321b4f66271f4983
tolfino <39185647+tolfino@users.noreply.github.com>
2021-01-17 20:49:12 -0800

    add changepfp for kicks

--- cringebot/bot.py
+++ cringebot/bot.py
@@ -1,9 +1,11 @@
 import os
 import discord
+import requests
+from discord.ext import commands
 from discord_slash import SlashCommand
 from discord_slash import SlashContext
 
-bot = discord.Client()
+bot = commands.Bot(command_prefix='!')
 slash = SlashCommand(bot)
 
 
@@ -27,6 +29,22 @@ async def clearnick(ctx: SlashContext, user: str):
         author = await bot.fetch_user(author)
     await bot.http.edit_member(ctx.guild.id, user, reason=prettyprint_author(author), nick=None)
     await ctx.ack()
+
+
+@bot.command()
+async def changepfp(ctx):
+    """change my pfp"""
+    if not ctx.message.attachments:
+        await ctx.send("you didn't attach an image.. cringe.......")
+        return
+
+    try:
+        await bot.user.edit(avatar=requests.get(ctx.message.attachments[0].url).content)
+    except Exception as e:
+        await ctx.send(f"oopsie woopsie there was a fucky wucky:\n```\n{e}```")
+        return
+
+    await ctx.send("ok.. fine")
 
 
 def main(token=os.getenv('CRINGEBOT_TOKEN')):
