0d7284d556b0e9465fe8eaa33b51a96d25fccbf9
tolfino <39185647+tolfino@users.noreply.github.com>
2021-01-23 19:28:06 -0800

    add cringe reply

--- cringebot/bot.py
+++ cringebot/bot.py
@@ -19,7 +19,7 @@ async def changenick(ctx: SlashContext, user: str, nick: str):
     if isinstance(author, int):
         author = await bot.fetch_user(author)
     await bot.http.edit_member(ctx.guild.id, user, reason=prettyprint_author(author), nick=nick)
-    await ctx.ack()
+    await ctx.send(f'nickname for <@{user}> set', hidden=True)
 
 
 @slash.slash(name='clearnick')
@@ -28,7 +28,7 @@ async def clearnick(ctx: SlashContext, user: str):
     if isinstance(author, int):
         author = await bot.fetch_user(author)
     await bot.http.edit_member(ctx.guild.id, user, reason=prettyprint_author(author), nick=None)
-    await ctx.ack()
+    await ctx.send(f'nickname for <@{user}> cleared', hidden=True)
 
 
 @bot.command()
